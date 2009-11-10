cda1519ed93ca50c2aaff7ad0e20f81ac8c63d2c
rmiddle <robert@middleswarth.net>
2009-11-10 15:36:13 -0500

    Adding in new plugin answernet.emailresponse.filters

---
+++ plugins/answernet.emailresponse.filters/app/app.php
@@ -0,0 +1,36 @@
+<?php
+
+class AnswernetMetlifeFilterPlugin extends DevblocksPlugin {};
+
+class AnswernetMetlifeFilterCopyAction extends Extension_MailFilterAction {
+	const EXTENSION_ID = 'answernet.metlife_filters.action.copy';
+
+	function __construct($manifest) {
+		$this->DevblocksExtension($manifest,1);
+	}
+
+	function run(Model_PreParseRule $filter, CerberusParserMessage $message) {
+		$ticket_fields = DAO_CustomField::getAll();
+		$params = $filter->actions[self::EXTENSION_ID];
+
+    $logger = DevblocksPlatform::getConsoleLog();
+    $logger->info(print_r($message));
+    $logger->info(print_r($filter));
+
+    // Houser,Colin <1034179><Missing Serviced Customer information>
+
+
+				// collapse multi-line headers to single line for single-line text fields
+				//if($ticket_fields[$custom_fields[$idx]]->type == Model_CustomField::TYPE_SINGLE_LINE) {
+				//	$message->custom_fields[$custom_fields[$idx]]
+				//		= trim(implode(' ',$value));
+				//} elseif($ticket_fields[$custom_fields[$idx]]->type == Model_CustomField::TYPE_MULTI_LINE) {
+				//	$message->custom_fields[$custom_fields[$idx]]
+				//		= trim(implode('\r\n',$value));
+	}
+
+	// function renderConfig(Model_PreParseRule $filter=null) {}
+
+	// function saveConfig() {}
+
+};
