bc643dd34dd89fc7c68ccf3ab765856d614b350b
rmiddle <robert@middleswarth.net>
2009-11-10 23:16:48 -0500

    debug

--- plugins/answernet.emailresponse.filters/app/app.php
+++ plugins/answernet.emailresponse.filters/app/app.php
@@ -30,35 +30,19 @@ class AnswernetMetlifeFilterCopyAction extends Extension_MailFilterAction {
     // 8 = Due Date
     // 9 = SLA
     // 
-    $message->body .= print_r($subject);
     $sub = explode(',', $subject, 2);
-    $message->body .= print_r($sub);
     $lname = $sub[0];
-    $message->body .= print_r($lname);
     $sub2 = explode("<", $sub[1]);
-    $message->body .= print_r($sub2);
     $fname = $sub2[0];
-    $message->body .= print_r($fname);
     $emp_id = $sub2[1];
-    $message->body .= print_r($emp_id);
     $type_custom = $sub2[2];
-    $message->body .= print_r($type_custom);
-//    $message->custom_fields['1'] = trim($fname) . trim($lname);
-//    $message->custom_fields['3'] = (int)$emp_id);
-//    $message->body .= "emp_id = " . $emp_id;
-//    $message->body .= "type = " . $type_custom;
-//    $message->body .= "fname = " . $fname;
-//    $message->body .= "lname = " . $lname;
-//    $message->body .= "sub = " . $sub;
-//    $message->body .= "sub2 = " . $sub2;
+    $message->custom_fields['1'] = trim($fname) . trim($lname);
+    $message->custom_fields['3'] = (int)$emp_id);
+    $message->body .= "emp_id = " . $emp_id;
+    $message->body .= "type = " . $type_custom;
+    $message->body .= "fname = " . $fname;
+    $message->body .= "lname = " . $lname;
 
-				// collapse multi-line headers to single line for single-line text fields
-				//if($ticket_fields[$custom_fields[$idx]]->type == Model_CustomField::TYPE_SINGLE_LINE) {
-				//	$message->custom_fields[$custom_fields[$idx]]
-				//		= trim(implode(' ',$value));
-				//} elseif($ticket_fields[$custom_fields[$idx]]->type == Model_CustomField::TYPE_MULTI_LINE) {
-				//	$message->custom_fields[$custom_fields[$idx]]
-				//		= trim(implode('\r\n',$value));
 	}
 
 	// function renderConfig(Model_PreParseRule $filter=null) {}
