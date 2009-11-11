e6c676b12afe7236acb3ec37264b33f365f61176
rmiddle <robert@middleswarth.net>
2009-11-10 19:30:23 -0500

    More work with Subject

--- plugins/answernet.emailresponse.filters/app/app.php
+++ plugins/answernet.emailresponse.filters/app/app.php
@@ -15,17 +15,14 @@ class AnswernetMetlifeFilterCopyAction extends Extension_MailFilterAction {
 		$ticket_fields = DAO_CustomField::getAll();
 		$params = $filter->actions[self::EXTENSION_ID];
 
-//    $logger = DevblocksPlatform::getConsoleLog();
-//    $logger->info("Answernet: Running Filter on New Mail");
+    $logger = DevblocksPlatform::getConsoleLog();
+    $logger->info("Answernet: Running Filter on New Mail");
 //    $logger->info(print_r($message_headers->subject));
-    echo "Answernet: Running Filter on New Mail<br>";
-    echo $subject;
-    echo "<br>";
 //    $logger->info(print_r($filter));
 //    $logger->info(print_r($ticket_fields));
 
     // Houser,Colin <1034179><Missing Serviced Customer information>
-
+    $message->custom_fields[1] = $subject;
 				// collapse multi-line headers to single line for single-line text fields
 				//if($ticket_fields[$custom_fields[$idx]]->type == Model_CustomField::TYPE_SINGLE_LINE) {
 				//	$message->custom_fields[$custom_fields[$idx]]
