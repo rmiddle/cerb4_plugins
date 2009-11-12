0815896557edd1e8fabd65ffdd817e175a14f9f7
rmiddle <robert@middleswarth.net>
2009-11-12 08:33:11 -0500

    More changes.

--- plugins/answernet.emailresponse.filters/app/app.php
+++ plugins/answernet.emailresponse.filters/app/app.php
@@ -16,10 +16,7 @@ class AnswernetMetlifeFilterCopyAction extends Extension_MailFilterAction {
 		$params = $filter->actions[self::EXTENSION_ID];
 
     $logger = DevblocksPlatform::getConsoleLog();
-    $logger->info("Answernet: Running Filter on New Mail");
-//    $logger->info(print_r($message_headers->subject));
-//    $logger->info(print_r($filter));
-//    $logger->info(print_r($ticket_fields));
+    $logger->info("Answernet: Running Filter on New Metlife Mail");
 
     // Houser,Colin <1034179><Missing Serviced Customer information>
     // Current custom_fields numbers
@@ -27,6 +24,14 @@ class AnswernetMetlifeFilterCopyAction extends Extension_MailFilterAction {
     // 2 = New Hire Yes = 0 / No = 2
     // 3 = RM Employee ID
     // 6 = Request Type /0/2/4/6/8/10/12/14
+    //    0 = Import contacts
+    //    2 = Create mailing list from existing data
+    //    4 = Update existing contacts
+    //    6 = Research missing customer info
+    //    8 = Create labels
+    //   10 = Fix duplicate contacts
+    //   12 = Export third-party file
+    //   14 = Other
     // 8 = Due Date
     // 9 = SLA
     // 
@@ -38,8 +43,20 @@ class AnswernetMetlifeFilterCopyAction extends Extension_MailFilterAction {
     $type_custom = $sub2[2];
     $message->custom_fields['1'] = trim($fname) . " " . trim($lname);
     $message->custom_fields['3'] = substr($emp_id, 0, -1);
-    $message->body .= "emp_id = " . substr($emp_id, 0, -1);
+
     $message->body .= "type = " . substr($type_custom, 0, -1);
+
+    // SLA
+    // 1 = Import contacts NEW HIRE in last 45 days
+    // 1 = Create Labels
+    // 3 = Import Contacts
+    // 3 = Create Mailing list from existing data
+    // 3 = Update existing contacts
+    // 3 = Fix duplicate contacts
+    // 3 = Export third-party file
+    // 5 = Missing or incorrect customer info
+    // 5 = Other
+
 	}
 
 	// function renderConfig(Model_PreParseRule $filter=null) {}
