44161b162359e8475f9844b719ddd6981f0e3f88
rmiddle <robert@middleswarth.net>
2009-11-13 01:28:44 -0500

    Adding the 6 other topics.  Those will be very easy to update.

--- plugins/answernet.emailresponse.filters/app/app.php
+++ plugins/answernet.emailresponse.filters/app/app.php
@@ -18,9 +18,6 @@ class AnswernetMetlifeFilterCopyAction extends Extension_MailFilterAction {
 
     $logger = DevblocksPlatform::getConsoleLog();
     $logger->info("Answernet: Running Filter on New Mail");
-//    $logger->info(print_r($message_headers->subject));
-//    $logger->info(print_r($filter));
-//    $logger->info(print_r($ticket_fields));
 
     // Houser,Colin <1034179><Missing Serviced Customer information>
     // Current custom_fields numbers
@@ -40,11 +37,79 @@ class AnswernetMetlifeFilterCopyAction extends Extension_MailFilterAction {
     $message->custom_fields['2'] = substr($emp_id, 0, -1);
     $message->custom_fields['3'] = trim($fname) . " " . trim($lname);
 
+    // If topic == Import contacts
+    if (preg_match('/import/i', $topic_metlife)) {
+      $message->custom_fields['4'] = "Import contacts";
+      $message->custom_fields['5'] = 1;
+      $message->custom_fields['6'] = "Yes";
+    }
+
+    // If topic == Create mailing list from existing data
+    if (preg_match('/mailing/i', $topic_metlife)) {
+      $message->custom_fields['4'] = "Create mailing list from existing data";
+      $message->custom_fields['5'] = 3;
+      $message->custom_fields['6'] = "Yes";
+    }
+
+    // If topic == Update existing contacts
+    if (preg_match('/Update/i', $topic_metlife)) {
+      $message->custom_fields['4'] = "Update existing contacts";
+      $message->custom_fields['5'] = 3;
+      $message->custom_fields['6'] = "No";
+    }
+
+    // If topic == Research missing customer info
+    if (preg_match('/Research/i', $topic_metlife)) {
+      $message->custom_fields['4'] = "Research missing customer info";
+      $message->custom_fields['5'] = 5;
+      $message->custom_fields['6'] = "No";
+    }
+
+    // If topic == Create labels
+    if (preg_match('/labels/i', $topic_metlife)) {
+      $message->custom_fields['4'] = "Create labels";
+      $message->custom_fields['5'] = 1;
+      $message->custom_fields['6'] = "No";
+    }
+
+    // If topic == Export third-party file
+    if (preg_match('/third/i', $topic_metlife)) {
+      $message->custom_fields['4'] = "Export third-party file";
+      $message->custom_fields['5'] = 3;
+      $message->custom_fields['6'] = "No";
+    }
+
     // If topic == Other
     if (preg_match('/Other/i', $topic_metlife)) {
       $message->custom_fields['4'] = "Other";
       $message->custom_fields['5'] = 5;
       $message->custom_fields['6'] = "No";
+    }
+
+    // SLA of 1.  Process day of week Busness Days suck.
+    if ($message->custom_fields['5'] == 1) {
+      if (($day_of_week < 5) || ($day_of_week == 7)) {
+        $message->custom_fields['1'] = strtotime("+1 Days");
+      }
+      if ($day_of_week == 6) {
+        $message->custom_fields['1'] = strtotime("+2 Days");
+      }
+      if ($day_of_week == 5) {
+        $message->custom_fields['1'] = strtotime("+3 Days");
+      }
+    }
+
+    // SLA of 3.  Process day of week Busness Days suck.
+    if ($message->custom_fields['5'] == 3) {
+      if (($day_of_week < 4) || ($day_of_week == 7)) {
+        $message->custom_fields['1'] = strtotime("+3 Days");
+      }
+      if ($day_of_week == 6) {
+        $message->custom_fields['1'] = strtotime("+4 Days");
+      }
+      if (($day_of_week > 3) && ($day_of_week < 6)) {
+        $message->custom_fields['1'] = strtotime("+5 Days");
+      }
     }
 
     // SLA of 5.  Process day of week Busness Days suck.
@@ -60,14 +125,6 @@ class AnswernetMetlifeFilterCopyAction extends Extension_MailFilterAction {
       }
     }
 	}
-
-// $date_var = strtotime("Last Monday");
-// for ($i = 1; $i <= 10; $i++) {
-//   $week_choices[$i]['time'] = $date_var;
-//   $week_choices[$i]['text'] = "Week of ".date("W: n/j/y", $date_var).'
-//- '.date("n/j/y", $date_var+518400);
-//      $date_var -= 604800;
-//    }
 
 	// function renderConfig(Model_PreParseRule $filter=null) {}
 
