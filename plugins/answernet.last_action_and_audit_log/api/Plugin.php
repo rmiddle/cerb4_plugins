fc7596b5a8fb9cde46f56a9d1a9eb95796f190f9
rmiddle <robert@middleswarth.net>
2010-01-04 21:18:27 -0500

    Fixed the plugin to work with 4.3.x

--- plugins/answernet.last_action_and_audit_log/api/Plugin.php
+++ plugins/answernet.last_action_and_audit_log/api/Plugin.php
@@ -1,9 +1,4 @@
 <?php
-class AnswernetLastActionAndAuditLogPlugin extends DevblocksPlugin {
-  function load(DevblocksPluginManifest $manifest) {
-  }
-};
-
 if (class_exists('DevblocksTranslationsExtension',true)):
 	class AnswernetLastActionAndAuditLogTranslations extends DevblocksTranslationsExtension {
 		function __construct($manifest) {
